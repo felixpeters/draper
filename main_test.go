5264435fb460a9e4e05bca3013a2b421fd18480a
Felix <felix@albrechtpeters.de>
2016-12-07 16:45:43 +0100

    go-simplejson for easier API testing

--- main_test.go
+++ main_test.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	simplejson "github.com/bitly/go-simplejson"
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_ResponseCode(t *testing.T) {
-	req, err := http.NewRequest("GET", "localhost:8080", nil)
+func checkError(err error, t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func Test_ResponseCode(t *testing.T) {
+	req, err := http.NewRequest("GET", "localhost:8080", nil)
+	checkError(err, t)
 
 	res := httptest.NewRecorder()
 	HelloWorld(res, req)
@@ -25,9 +30,7 @@ func Test_ResponseCode(t *testing.T) {
 
 func Test_HelloWorld(t *testing.T) {
 	req, err := http.NewRequest("GET", "localhost:8080", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	checkError(err, t)
 
 	res := httptest.NewRecorder()
 	HelloWorld(res, req)
@@ -39,9 +42,7 @@ func Test_HelloWorld(t *testing.T) {
 
 func Test_JsonHelloWorld(t *testing.T) {
 	req, err := http.NewRequest("GET", "localhost:8080/json", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	checkError(err, t)
 
 	res := httptest.NewRecorder()
 	JsonHelloWorld(res, req)
@@ -49,8 +50,11 @@ func Test_JsonHelloWorld(t *testing.T) {
 	assert.Equal(t, 200, res.Code, "Response code should be 200")
 	assert.JSONEq(t, `{ "success": true, "message": "Hello World" }`, res.Body.String(), "Response body should be JSON")
 
-	var sr SimpleResponse
-	json.NewDecoder(res.Body).Decode(&sr)
-	assert.True(t, sr.Success, "Success value should be true")
-	assert.Equal(t, "Hello World", sr.Message, "Message should be Hello World")
+	body, err := ioutil.ReadAll(res.Body)
+	checkError(err, t)
+	js, err := simplejson.NewJson(body)
+	checkError(err, t)
+
+	assert.True(t, js.Get("success").MustBool(), "Success value should be true")
+	assert.Equal(t, "Hello World", js.Get("message").MustString(), "Message should be Hello World")
 }
