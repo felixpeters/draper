daee1f6a1b0ac53aa9d58b622c5ac2c57b1de106
Felix <felix@albrechtpeters.de>
2016-12-14 12:30:22 +0100

    Request logger now includes body size and status code

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM golang
 ADD . /go/src/github.com/felixpeters/draper 
 # Install the app
 RUN go install github.com/felixpeters/draper
-# Run the binary
-ENTRYPOINT /go/bin/draper
 # Expose port 8080
 EXPOSE 8080
+# Run the binary
+CMD ["/go/bin/draper"]
--- main.go
+++ main.go
@@ -16,6 +16,71 @@ type SimpleResponse struct {
 	Message string `json:"message"`
 }
 
+type loggingHandler struct {
+	logger  log.Logger
+	handler http.Handler
+}
+
+func (h loggingHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	t1 := time.Now()
+	logrw := NewLoggingResponseWriter(w)
+	h.handler.ServeHTTP(logrw, req)
+	t2 := time.Now()
+	h.logger.Log("method", req.Method, "url", req.URL.String(), "responseTime", t2.Sub(t1), "code", logrw.Status(), "size", logrw.Size())
+}
+
+func NewLoggingResponseWriter(w http.ResponseWriter) loggingResponseWriter {
+	var logger loggingResponseWriter = &responseLogger{w: w}
+	return logger
+}
+
+type loggingResponseWriter interface {
+	http.ResponseWriter
+	http.Flusher
+	Status() int
+	Size() int
+}
+
+type responseLogger struct {
+	w      http.ResponseWriter
+	status int
+	size   int
+}
+
+func (l *responseLogger) Header() http.Header {
+	return l.w.Header()
+}
+
+func (l *responseLogger) Write(b []byte) (int, error) {
+	if l.status == 0 {
+		// The status will be StatusOK if WriteHeader has not been called yet
+		l.status = http.StatusOK
+	}
+	size, err := l.w.Write(b)
+	l.size += size
+	return size, err
+}
+
+func (l *responseLogger) WriteHeader(s int) {
+	l.w.WriteHeader(s)
+	l.status = s
+}
+
+func (l *responseLogger) Status() int {
+	return l.status
+}
+
+func (l *responseLogger) Size() int {
+	return l.size
+}
+
+func (l *responseLogger) Flush() {
+	f, ok := l.w.(http.Flusher)
+	if ok {
+		f.Flush()
+	}
+}
+
 var logger log.Logger
 
 func init() {
@@ -24,15 +89,8 @@ func init() {
 	logger = log.NewContext(logger).With("ts", log.DefaultTimestamp, "caller", log.DefaultCaller)
 }
 
-func loggingHandler(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		t1 := time.Now()
-		next.ServeHTTP(w, r)
-		t2 := time.Now()
-		logger.Log("method", r.Method, "url", r.URL.String(), "responseTime", t2.Sub(t1))
-	}
-
-	return http.HandlerFunc(fn)
+func LoggingHandler(l log.Logger, h http.Handler) http.Handler {
+	return loggingHandler{l, h}
 }
 
 func recoverHandler(next http.Handler) http.Handler {
@@ -56,6 +114,7 @@ func JsonHelloWorld(w http.ResponseWriter, r *http.Request) {
 }
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, "Hello World")
 }
 
@@ -64,7 +123,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HelloWorld)
 	r.HandleFunc("/json", JsonHelloWorld)
-	loggedRouter := loggingHandler(recoverHandler(r))
+	loggedRouter := LoggingHandler(logger, recoverHandler(r))
 
 	srv := &http.Server{
 		Handler:      loggedRouter,
